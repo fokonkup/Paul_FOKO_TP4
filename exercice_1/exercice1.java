fb37dd159c038783ad99927ddb45daa40d7e3635
paulfoko21 <wilfriedngueguim@gmail.com>
2024-11-26 15:00:27 +0100

    optimisation du code de la classe FactureManager

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,18 +1,31 @@
+/*
+ * classe de gestion de facture avec les methodes de calcul de remise
+ */
 public class FactureManager {
+    /*
+     * calcul de la reduction selon la categorie
+     * @return du montant avec la reduction appliquer
+     */
+    public double getReductuion(String typeProduit, double mount){
+        if (typeProduit.equals("Alimentaire")) {
+            mount -= mount * 0.05; // Réduction de 5%
+        } else if (typeProduit.equals("Electronique")) {
+            mount -= mount * 0.1; // Réduction de 10%
+        } else if (typeProduit.equals("Luxe")) {
+            mount -= mount * 0.15; // Réduction de 15%
+        }
 
-    // Calcul le total d'une facture
+        return mount;
+    }
+    
+    /*
+     * Calcul le total d'une facture
+     * @return de montant de la facture total
+     */    
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
 
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
-        }
-
+        total = getReductuion(typeProduit, total);
         // Reduction sur le total
         if (total > 1000) {
             total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
